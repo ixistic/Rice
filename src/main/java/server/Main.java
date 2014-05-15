46193cfafd601799517047027a5916675cd808a9
mapfap <mapkabb@gmail.com>
2014-05-16 04:00:38 +0700

    Dist .jar

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -4,8 +4,13 @@ import com.sample.Drools;
 
 public class Main {
 	public static final void main(String[] args) {
+		int port = 5000;
+		if ( args.length >= 1 ) {
+			port = Integer.parseInt(args[0]);
+		}
+		
 		final Drools drools = new Drools();
-		new ESServer(5000, new ExpertSystem() {
+		new ESServer(port, new ExpertSystem() {
 			@Override
 			public String getAnswer(String question) {
 				String answer = drools.getAnswer(question);
